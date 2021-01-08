d517e538671d32011ac1b566400e15b2a4e7772b
wlnirvana <>
2021-01-08 13:04:28 +0800

    initial commit

---
+++ src/YouNameItConstants.java
@@ -0,0 +1,14 @@
+/*
+ * 定义了一些常量，可以被子类所使用
+ */
+
+public interface YouNameItConstants {
+
+    int APPLICATION_WIDTH = 800;
+
+    int APPLICATION_HEIGHT = 600;
+
+    String NAMES_DATA_FILE = "names-data.txt";
+
+    int TEXT_FIELD_WIDTH = 16;
+}
